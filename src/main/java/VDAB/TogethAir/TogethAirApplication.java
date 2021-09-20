fe6b8f84518171026bf9df4f053e71255aae1988
Tom Van Laeken <tototonique@gmail.com>
2021-09-20 10:54:13 +0200

    Backend Build and source

---
+++ src/main/java/VDAB/TogethAir/TogethAirApplication.java
@@ -0,0 +1,13 @@
+package VDAB.TogethAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogethAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogethAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VDAB/TogethAir/TogethAirApplicationTests.java
@@ -0,0 +1,13 @@
+package VDAB.TogethAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogethAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
