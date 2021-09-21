c2e118ba1812d0c3cdd53c7762b90c68c2be8e6f
Deadalus64 <kaan0serin@gmail.com>
2021-09-21 09:56:35 +0200

    closes #9

---
+++ src/main/java/VDAB/TogethAir/repository/FlightRepository.java
@@ -0,0 +1,7 @@
+package VDAB.TogethAir.repository;
+
+import VDAB.TogethAir.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightRepository extends JpaRepository<Flight, Long>{
+}
--- src/main/java/VDAB/TogethAir/repository/PersonRepository.java
+++ src/main/java/VDAB/TogethAir/repository/PersonRepository.java
@@ -4,5 +4,5 @@ package VDAB.TogethAir.repository;
 import VDAB.TogethAir.model.user.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface repository extends JpaRepository<Long, Person>{
+public interface PersonRepository extends JpaRepository<Person, Long>{
 }
