c546530f45b494b81f79f581fece632ee33fb090
John Wilkos <johnwilkosmail@gmail.com>
2021-09-21 10:22:29 +0200

    Added PersonService. Closes #11

---
+++ src/main/java/VDAB/TogethAir/services/PersonService.java
@@ -0,0 +1,33 @@
+package VDAB.TogethAir.services;
+
+import VDAB.TogethAir.model.user.Person;
+import VDAB.TogethAir.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class PersonService {
+    private final PersonRepository personRepository;
+
+    public Person addPerson(Person person){
+        return personRepository.save(person);
+    }
+
+    public Person updatePerson(Person person){
+        return personRepository.save(person);
+    }
+
+    public Person findPersonById(Long id) throws Exception {
+        return personRepository.findById(id)
+                .orElseThrow(()-> new Exception("User by id " + id + " was not found."));
+    }
+
+    @Transactional
+    public void deletePerson(Long id){
+        personRepository.deleteById(id);
+    }
+
+}
