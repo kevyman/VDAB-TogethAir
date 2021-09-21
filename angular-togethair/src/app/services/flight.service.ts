05bcad9847bf0b7acf84c1328094af478f945520
Youri Vanderleyden <77174749+YVDL@users.noreply.github.com>
2021-09-21 14:28:47 +0200

    added flightservice.ts commit closes #25

---
+++ angular-togethair/src/app/services/flight.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import {Flight} from "./flight";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService{
+  private apiServerUrl =  environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getFlights(): Observable<Flight[]>{
+    return this.http.get<Flight[]>(`${this.apiServerUrl}/allFlights`)
+  }
+
+  public findFlightById(id:number): Observable<Flight>{
+    return this.http.get<Flight>(`${this.apiServerUrl}/findFlight/${id}`)
+  }
+
+  public addFlight(flight: Flight): Observable<Flight>{
+    return this.http.post<Flight>(`${this.apiServerUrl}/addFlight`, flight)
+  }
+
+  public updateFlight(flight:Flight): Observable<Flight>{
+    return this.http.put<Flight>(`${this.apiServerUrl}/updateFlight`, flight)
+  }
+
+  public deleteFlight(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/deleteFlight/${id}`)
+  }
+}
