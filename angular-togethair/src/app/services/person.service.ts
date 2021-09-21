4634a8e21fa7b89c814edd295fa294499a6c7b29
John Wilkos <johnwilkosmail@gmail.com>
2021-09-21 14:21:19 +0200

    Added person.service.ts closes #24

---
+++ angular-togethair/src/app/services/person.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Person } from "./person";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PersonService{
+    private apiServerUrl =  environment.apiBaseUrl;
+
+    constructor(private http: HttpClient){}
+
+    public findPersonById(id:number): Observable<Person>{
+        return this.http.get<Person>(`${this.apiServerUrl}/findPerson/${id}`)
+    }
+
+    public addPerson(person: Person): Observable<Person>{
+        return this.http.post<Person>(`${this.apiServerUrl}/addPerson`, person)
+    }
+
+    public updatePerson(person: Person): Observable<Person>{
+        return this.http.put<Person>(`${this.apiServerUrl}/updatePerson`, person)
+    }
+
+    public deletePerson(id: number): Observable<void>{
+        return this.http.delete<void>(`${this.apiServerUrl}/deletePerson/${id}`)
+    }
+}
+
