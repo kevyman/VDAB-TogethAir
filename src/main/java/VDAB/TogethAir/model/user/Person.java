0accfe18034a1f7b075dc24839d90ea6fe1130c6
Tom Van Laeken <tototonique@gmail.com>
2021-09-22 11:10:33 +0200

    Fixed Abstract super bug

--- src/main/java/VDAB/TogethAir/model/user/Person.java
+++ src/main/java/VDAB/TogethAir/model/user/Person.java
@@ -4,10 +4,8 @@ import VDAB.TogethAir.model.AbstractSuper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -15,7 +13,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user" , schema = "togethair")
-public class Person extends AbstractSuper  implements Serializable {
+public class Person extends AbstractSuper implements Serializable {
 
     @Column(nullable = false , length = 100)
     private String name;
