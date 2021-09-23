179541e7683f48fa29a13da4a9075abf3df40caa
Tom Van Laeken <tototonique@gmail.com>
2021-09-23 12:51:23 +0200

    Added angular model airports.ts closes #58

---
+++ angular-togethair/src/app/models/airports.ts
@@ -0,0 +1,13 @@
+export interface Airports{
+  code: string;
+  name: string;
+  cityCode: string;
+  cityName: string;
+  countryName: string;
+  countryCode: string;
+  timezone: string;
+  lat: string;
+  lon: string;
+  numAirports: number;
+  city: string;
+}
