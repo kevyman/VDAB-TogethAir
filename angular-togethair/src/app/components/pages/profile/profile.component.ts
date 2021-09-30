ffdda149a5ae0577bf35e300cd315496f5571500
Tom Van Laeken <tototonique@gmail.com>
2021-09-30 10:11:07 +0200

    Added Button in the header for loggin in and out

---
+++ angular-togethair/src/app/components/pages/profile/profile.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+})
+export class ProfileComponent implements OnInit {
+  profileJson !: string;
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2))
+    );
+  }
+}
