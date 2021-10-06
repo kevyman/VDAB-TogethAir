7d4067afc150446ca453af469333ef56a742557c
Tom Van Laeken <tototonique@gmail.com>
2021-10-06 15:54:51 +0200

    added booking service , controller and repository closes #137

---
+++ src/main/java/VDAB/TogethAir/services/BookingService.java
@@ -0,0 +1,33 @@
+package VDAB.TogethAir.services;
+
+import VDAB.TogethAir.model.Booking;
+import VDAB.TogethAir.repository.BookingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class BookingService {
+
+    private BookingRepository bookingRepository;
+
+    public Booking addBooking(Booking booking){
+        return bookingRepository.save(booking);
+    }
+
+    public Booking updateBooking(Booking booking){
+        return bookingRepository.save(booking);
+    }
+
+    public Booking findBookingById(Long id) throws Exception {
+        return bookingRepository.findById(id)
+                .orElseThrow(()-> new Exception("User by id " + id + " was not found."));
+    }
+
+    @Transactional
+    public void deleteBooking(Long id){
+        bookingRepository.deleteById(id);
+    }
+}
