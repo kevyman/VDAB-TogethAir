f3b4f08d408459c410b0936f2bb1e83f3ad274e8
Tom Van Laeken <tototonique@gmail.com>
2021-10-06 14:34:50 +0200

    Fixed the buttons disappearing 'Kind of'

--- angular-togethair/src/app/components/header/header.component.ts
+++ angular-togethair/src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {AuthService} from "@auth0/auth0-angular";
 import {DOCUMENT} from "@angular/common";
 import {PersonService} from "../../services/person.service";
+import {Person} from "../../models/person";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-header',
@@ -10,11 +12,20 @@ import {PersonService} from "../../services/person.service";
 })
 export class HeaderComponent implements OnInit {
 
+  public userObj : any = this.auth.user$.subscribe(userObj => this.userObj = userObj);
+  public person: Person = {emailAddress: "", role: ""};
+
   constructor(public auth: AuthService,
               @Inject(DOCUMENT) private doc : Document,
               private personService:PersonService) { }
 
   ngOnInit(): void {
+    if (this.userObj?.email) {
+      this.personService.findPersonByEmailAddress(this.userObj.email).subscribe(person => {
+      this.person = person;
+      console.log(this.person.role);
+     });
+    }
   }
 
   logout():void{
@@ -23,5 +34,9 @@ export class HeaderComponent implements OnInit {
 
   loginWithRedirect():void{
     this.auth.loginWithRedirect();
+  }
+
+  private tempFunc() {
+    return this.auth.user$;
   }
 }
