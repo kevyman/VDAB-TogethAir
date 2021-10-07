01ac56fef1998c1872cc3dfec0097382bc4757a6
John Wilkos <johnwilkosmail@gmail.com>
2021-10-07 17:12:18 +0200

    Added flight info to cart, and adult and children update functionality. Closes #127

--- angular-togethair/src/app/components/cart/cart.component.ts
+++ angular-togethair/src/app/components/cart/cart.component.ts
@@ -13,6 +13,9 @@ export class CartComponent implements OnInit {
   childPrice: number;
   totalPrice: number;
   bookingFee: number;
+  kids: number;
+  grownups: number;
+
 
   constructor() { }
 
@@ -28,6 +31,47 @@ export class CartComponent implements OnInit {
     this.childPrice = this.bookFlight.children * this.bookFlight.price * .8; //kids get 20% discount
     this.bookingFee = (this.adultPrice+this.childPrice)*.05;
     this.totalPrice = this.adultPrice + this.childPrice + this.bookingFee;
+    this.kids = this.bookFlight.children;
+    this.grownups = this.bookFlight.adults;
+  }
+
+  didItChange(){
+    console.log("Kids: " + this.kids);
+    console.log("Adults: " + this.grownups);
+  }
+
+  onNumberChange(){
+    this.bookFlight.children = this.kids;
+    this.bookFlight.adults = this.grownups;
+    this.adultPrice = this.bookFlight.adults * this.bookFlight.price;
+    this.childPrice = this.bookFlight.children * this.bookFlight.price * .8; //kids get 20% discount
+    this.bookingFee = (this.adultPrice+this.childPrice)*.05;
+    this.totalPrice = this.adultPrice + this.childPrice + this.bookingFee;
+  }
+
+  childInc(){
+    this.kids++;
+    this.onNumberChange();
+  }
+
+  childDec(){
+    if(this.kids>0){
+      this.kids--;
+      this.onNumberChange();
+    }
+  }
+
+  adultInc(){
+    this.grownups++;
+    this.onNumberChange();
+  }
+
+  adultDec(){
+    if(this.grownups>0){
+      this.grownups--;
+      this.onNumberChange();
+    }
+    
   }
 
 }
