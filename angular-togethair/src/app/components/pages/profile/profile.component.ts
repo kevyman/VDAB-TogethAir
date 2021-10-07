fe7ecbaa6595af78a3e26e9417989a11baa2b088
Tom Van Laeken <tototonique@gmail.com>
2021-10-07 19:09:37 +0200

    Changed the profile component (html)

--- angular-togethair/src/app/components/pages/profile/profile.component.ts
+++ angular-togethair/src/app/components/pages/profile/profile.component.ts
@@ -1,19 +1,31 @@
 
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import {PersonService} from "../../../services/person.service";
+import {Person} from "../../../models/person";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
-  profileJson !: string;
 
-  constructor(public auth: AuthService) {}
+  public userObj !: any;
+
+  public person: Person = {id: 0, emailAddress: "", role: ""};
+
+
+  constructor(public auth: AuthService,
+              public personService: PersonService) {}
 
   ngOnInit(): void {
-    this.auth.user$.subscribe(
-      (profile) => (this.profileJson = JSON.stringify(profile, null, 2))
-    );
+    this.auth.user$.subscribe(userObj => {
+      this.userObj = userObj
+      if (userObj) {
+        this.personService.findPersonByEmailAddress(this.userObj.email).subscribe(person => {
+          this.person = person;
+        });
+      }
+    });
   }
 }
